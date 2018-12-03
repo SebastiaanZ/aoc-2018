d85754c5cb04402e0e6af22f6f7c8daeae475049
SebastiaanZ <33516116+SebastiaanZ@users.noreply.github.com>
2018-12-03 19:37:24 +0100

    Added shortcutting skip to number_generator to speed up part I

--- previous_editions/2017-day15/aoc2017-day15.py
+++ previous_editions/2017-day15/aoc2017-day15.py
@@ -1,21 +1,21 @@
-def number_generator(start: int, factor: int, modulus: int=1) -> int:
+def number_generator(start: int, factor: int, mod: int, skip: bool) -> int:
     """Generator that yields the last 16 bits of the next number"""
     number = start
     while True:
         number = number * factor % 2147483647
-        if not number % modulus:
+        if skip or not number % mod:
             yield number & 65535
 
 
 if __name__ == "__main__":
-    a1 = number_generator(722, 16807, 1)
-    b1 = number_generator(354, 48271, 1)
+    a1 = number_generator(722, 16807, 1, True)
+    b1 = number_generator(354, 48271, 1, True)
 
     part1 = sum(next(a1) == next(b1) for i in range(4*10**7))
     print(f"In part 1, the judge counts: {part1}")
 
-    a2 = number_generator(722, 16807, 4)
-    b2 = number_generator(354, 48271, 8)
+    a2 = number_generator(722, 16807, 4, skip_mod=False)
+    b2 = number_generator(354, 48271, 8, skip_mod=False)
 
     part2 = sum(next(a2) == next(b2) for i in range(5*10**6))
     print(f"In part 2, the judge counts: {part2}")
