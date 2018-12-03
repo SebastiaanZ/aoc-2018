3d67d82ecc25333ba633284512ff90b670f5fb84
SebastiaanZ <33516116+SebastiaanZ@users.noreply.github.com>
2018-12-03 08:35:05 +0100

    Added docstrings and output formatting

--- day3/aoc-day3.py
+++ day3/aoc-day3.py
@@ -1,7 +1,12 @@
 import collections
 
 
-def extract_coordinates(iterable):
+def extract_coordinates(iterable) -> tuple:
+    """Takes iterable with claims in the form of:
+            '#1 @ 906,735: 28x17\n'
+       Extracts ID number, coordinates, size
+       Returns it as tuple of tuples
+    """
     for label in iterable:
         number, _, specs = label.partition(" @ ")
         coor, _, size = specs.partition(": ")
@@ -10,7 +15,8 @@ def extract_coordinates(iterable):
         yield number[1:], (int(x), int(y)), (int(width), int(height))
 
 
-def day3_part1(fn):
+def day3_part1(fn: str) -> int:
+    """Calculates the number of squares that have overlap from a claim file"""
     with open(fn) as f:
         used = {}
         for _, (x, y), (width, height) in extract_coordinates(f):
@@ -18,26 +24,32 @@ def day3_part1(fn):
                 for y1 in range(y, y+height):
                     used[(x1, y1)] = used.setdefault((x1, y1), 0) + 1
         squares = sum(1 for square in used.values() if square >= 2)
-        print(squares)
+        return squares
 
 
-def day3_part2(fn):
+def day3_part2(fn: str) -> int:
+    """Finds the claim that has no overlap with other claims in a claim file"""
     with open(fn) as f:
-        used = collections.defaultdict(list)
+        claims_on_square = collections.defaultdict(list)
         labels = {}
         for number, (x, y), (width, height) in extract_coordinates(f):
             labels[number] = True
             for x1 in range(x, x+width):
                 for y1 in range(y, y+height):
-                    used[(x1, y1)].append(number)
-                    if len(used[(x1, y1)]) > 1:
-                        for item in used[(x1, y1)]:
-                            labels[item] = False
+                    claims_on_square[(x1, y1)].append(number)
+
+                    if len(claims_on_square[(x1, y1)]) > 1:
+                        for claim in claims_on_square[(x1, y1)]:
+                            labels[claim] = False
 
         for key, value in labels.items():
             if value:
-                print(key)
+                return key
 
 
-day3_part1("day3-input.txt")
-day3_part2("day3-input.txt")
+if __name__ == "__main__":
+    overlap_squares = day3_part1("day3-input.txt")
+    claim_without_overlap = day3_part2("day3-input.txt")
+
+    print(f"The number o squares with overlap: {overlap_squares}")
+    print(f"Claim with no overlap: {claim_without_overlap}")
