56a1e480be02d6eda0a37f6a13e9e563a390280b
SebastiaanZ <33516116+SebastiaanZ@users.noreply.github.com>
2018-12-05 17:41:52 +0100

    Implemented a stack version of part 1

--- day5/aoc-day5.py
+++ day5/aoc-day5.py
@@ -1,9 +1,6 @@
 from string import ascii_lowercase as low
 from string import ascii_uppercase as upp
-
-
-with open('day5-input.txt') as f:
-    chemicals = f.read().strip()
+from collections import deque
 
 
 def get_reacting_couples():
@@ -28,13 +25,32 @@ def part_one(chemicals):
     return old_length
 
 
+def part_one2(chemicals):
+    d = deque()
+    for chemical in chemicals:
+        if not d:
+            d.append(chemical)
+        elif d[-1] == chemical.swapcase():
+            d.pop()
+        else:
+            d.append(chemical)
+    return len(d)
+
+
 def part_two(c):
     return min(part_one(c.replace(a, "").replace(b, "")) for a, b in zip(low, upp))
 
 
+def part_two2(c):
+    return min(part_one2(c.replace(a, "").replace(b, "")) for a, b in zip(low, upp))
+
+
 if __name__ == "__main__":
-    answer1 = part_one(chemicals)
+    with open('day5-input.txt') as f:
+        chemicals = f.read().strip()
+
+    answer1 = part_one2(chemicals)
     print(f"After the reaction, the length is: {answer1}")
 
-    answer2 = part_two(chemicals)
+    answer2 = part_two2(chemicals)
     print(f"Post-reaction length after removing offender: {answer2}")
