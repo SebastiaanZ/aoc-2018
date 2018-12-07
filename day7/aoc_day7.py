48a1d3a74b19280477b57d0bb205ae59ebb08ddc
SebastiaanZ <33516116+SebastiaanZ@users.noreply.github.com>
2018-12-07 20:49:31 +0100

    Introducing Manager and Tasklist class instead of abusing class variables

--- day7/aoc_day7.py
+++ day7/aoc_day7.py
@@ -1,50 +1,48 @@
 from string import ascii_uppercase
-from workforce import Workforce, Task
+from workforce import Manager, Tasklist
 
 
 def work(fn: str, worker_ids: str, n_workers: int=1, base_effort: int=0) -> tuple:
     with open(fn) as f:
         instructions = f.readlines()
-        to_do = Task.create_tasklist(worker_ids,
-                                     instructions,
-                                     base_effort)
+        tasklist = Tasklist(worker_ids,
+                            instructions,
+                            base_effort)
 
-    Workforce.recruite_workers(n_workers)
+    manager = Manager(n_workers)
+
     time = 0
-
-    while to_do:
-        while Workforce.free_workers:
-            to_do.sort()
-            task = to_do[0]
-            if not task.requirements:
-                worker = Workforce.get_free_worker()
-                worker.start_task(task, time)
-                to_do.pop(0)
+    while tasklist:
+        while manager.has_free_workers:
+            if tasklist.has_available_tasks:
+                task = tasklist.available_task
+                manager.employ_free_worker(task, time)
             else:
-                time = Workforce.finish_task()
-            if not to_do:
+                time = manager.finish_task()
+            if not tasklist:
                 break
         else:
-            time = Workforce.finish_task()
+            time = manager.finish_task()
     else:
-        while Workforce.assigned_workers:
-            time = Workforce.finish_task()
+        while manager.assigned_workers:
+            time = manager.finish_task()
 
-    return "".join(Task.done_order), time
+    return "".join(tasklist.done_order), time
 
 
 if __name__ == "__main__":
-    answer1 = work("day7-input.txt",
-                   ascii_uppercase,
-                   n_workers=1,
-                   base_effort=0)[0]
-    answer2 = work("day7-input.txt",
-                   ascii_uppercase,
-                   n_workers=5,
-                   base_effort=60)[1]
+    answer1, time = work("day7-input.txt",
+                         ascii_uppercase,
+                         n_workers=1,
+                         base_effort=0)
+
+    order, answer2 = work("day7-input.txt",
+                          ascii_uppercase,
+                          n_workers=5,
+                          base_effort=60)
 
     assert(answer1 == "GRTAHKLQVYWXMUBCZPIJFEDNSO")
     assert(answer2 == 1115)
 
-    print(f"Part I Task order: {answer1}")
-    print(f"Part II completion time: {answer2}")
+    print(f"Part I  task order: {answer1}    time: {time}")
+    print(f"Part II task order: {order}    time: {answer2}")
