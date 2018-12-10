df3cfd88331dc9c495f241740ed5be6cd8ecf861
SebastiaanZ <33516116+SebastiaanZ@users.noreply.github.com>
2018-12-10 09:08:18 +0100

    Restructured the code, removed unnecassary lines

--- day10/aoc-day10.py
+++ day10/aoc-day10.py
@@ -9,31 +9,30 @@ from scipy.spatial import distance
 from scipy.optimize import minimize
 import matplotlib.pyplot as plt
 
-coordinates = np.zeros((2, 322))
-
-with open("day10-input.txt") as f:
-    lines = list(f)
-    coor = np.zeros((len(lines), 2), dtype=int)
-    velo = np.zeros((len(lines), 2), dtype=int)
-    for row, line in enumerate(lines):
-        coor[row, :] = tuple(int(n) for n in line[10:24].split(", "))
-        velo[row, :] = tuple(int(n) for n in line[-8:-2].split(", "))
-
 
 def cityblock_sum(t):
     return distance.pdist(coor + t * velo).sum()
 
 
-min_mean = (coor.min(axis=0) // velo.min(axis=0)).mean()
-max_mean = (coor.max(axis=0) // velo.max(axis=0)).mean()
+if __name__ == "__main__":
+    with open("day10-input.txt") as f:
+        lines = list(f)
+        coor = np.zeros((len(lines), 2), dtype=int)
+        velo = np.zeros((len(lines), 2), dtype=int)
+        for row, line in enumerate(lines):
+            coor[row, :] = tuple(int(n) for n in line[10:24].split(", "))
+            velo[row, :] = tuple(int(n) for n in line[-8:-2].split(", "))
 
-guess = (min_mean + max_mean)//2
+    min_mean = (coor.min(axis=0) // velo.min(axis=0)).mean()
+    max_mean = (coor.max(axis=0) // velo.max(axis=0)).mean()
 
-second = np.round(minimize(cityblock_sum, guess).x)
+    guess = (min_mean + max_mean)//2
 
-print(second)
+    second = np.round(minimize(cityblock_sum, guess).x)
 
-coor2 = coor + second * velo
+    print(second)
 
-plt.plot(coor2[:, 0], -1*coor2[:, 1], 'ro')
-plt.show()
+    coor2 = coor + second * velo
+
+    plt.plot(coor2[:, 0], -1*coor2[:, 1], 'ro')
+    plt.show()
